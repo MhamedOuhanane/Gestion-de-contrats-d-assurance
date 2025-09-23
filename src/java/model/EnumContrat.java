a6f2d12ef5cbf26e352444d027d317f4ea1cc47d
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-23 16:10:21 +0100

    GDCD-7 Cree les enumerations necessaire pour le contrat et sinistre

--- src/java/model/EnumContrat.java
+++ src/java/model/EnumContrat.java
@@ -1,4 +1,17 @@
 package java.model;
 
 public enum EnumContrat {
+    MALADIE("Maladie"),
+    IMMOBILIER("Immobilier"),
+    AUTOMOBILE("Automobile");
+
+    private final String description;
+
+    EnumContrat(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- src/java/model/EnumSinistre.java
+++ src/java/model/EnumSinistre.java
@@ -1,4 +1,17 @@
 package java.model;
 
 public enum EnumSinistre {
+    MALADIE("maladie"),
+    MAISON("accident de maison"),
+    VOITURE("accident de voiture");
+
+    private final String description;
+
+    EnumSinistre(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
