b91c2ad04f63a56e0543cc6b106d982a1fdf9a04
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-23 16:43:33 +0100

    GDCD-7 Cree toutes les modeles avec les gettes et les setters

--- src/java/model/Sinistre.java
+++ src/java/model/Sinistre.java
@@ -1,4 +1,60 @@
 package java.model;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Sinistre {
+    private Integer id;
+    private EnumSinistre typeSinistre;
+    private LocalDate date;
+    private Double montant;
+    private String description;
+
+    public Sinistre(Integer id, EnumSinistre typeSinistre, LocalDate date, Double montant, String description) {
+        this.id = id;
+        this.typeSinistre = typeSinistre;
+        this.date = date;
+        this.montant = montant;
+        this.description = description;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTypeSinistre(EnumSinistre typeSinistre) {
+        this.typeSinistre = typeSinistre;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setMontant(Double montant) {
+        this.montant = montant;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public EnumSinistre getTypeSinistre() {
+        return typeSinistre;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Double getMontant() {
+        return montant;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
