9b34ec62f436d2a9aa03a7bd3af73ffe0144dec2
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-23 14:35:21 +0100

    GDCD-6 Cree les tables nécessaires de la base de donner du projet

---
+++ src/resources/shema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS conseiller (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS client(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    conseiller_id INT NOT NULL,
+    FOREIGN KEY (conseiller_id) REFERENCES conseiller(id)
+);
+
+CREATE TABLE IF NOT EXISTS contrat(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    typeContrat ENUM("Automobile", "Immobilier", "Maladie"),
+    dateDebut DATE NOT NULL,
+    dateFin DATE NOT NULL,
+    client_id INT NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES client(id)
+);
+
+CREATE TABLE IF NOT EXISTS sinistre (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    typeSinistre ENUM("accident de voiture", "accident de maison", 'maladie'),
+    date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    montant DECIMAL(10,2) NOT NULL,
+    description TEXT NOT NULL,
+    contrat_id INT NOT NULL,
+    FOREIGN KEY (contrat_id) REFERENCES contrat(id)
+);
