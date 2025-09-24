6d2a378196ae36de7aecf7d49fdb6e1e14a41210
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-24 17:16:49 +0100

    GDCD-11 Terminer la selection, suppression et find pour le conseiller dans la couche DAO

--- src/app/repository/impl/ConseillerRepositoryImpl.java
+++ src/app/repository/impl/ConseillerRepositoryImpl.java
@@ -10,8 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
 
 public class ConseillerRepositoryImpl implements ConseillerRepository {
     private static final Connection conn = DatabaseConfig.getInstance().getConnection();
@@ -44,16 +43,61 @@ public class ConseillerRepositoryImpl implements ConseillerRepository {
 
     @Override
     public Conseiller findConseiller(Integer conseiller_id) {
-        return null;
+        String selectQuery = "SELECT * FROM conseiller WHERE id = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(selectQuery)) {
+            stmt.setInt(1, conseiller_id);
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String nom = resultSet.getString("nom");
+                String prenom = resultSet.getString("prenom");
+                String email = resultSet.getString("email");
+
+                return new Conseiller(id, nom, prenom, email);
+            }
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de trouver le conseiller: " + e.getMessage());
+            return null;
+        }
     }
 
     @Override
-    public List<Conseiller> getAllConseiller() {
-        return Collections.emptyList();
+    public HashMap<Integer, Conseiller> getAllConseiller() {
+        String selectQuery = "SELECT * FROM conseiller";
+        try (PreparedStatement stmt = conn.prepareStatement(selectQuery)) {
+            ResultSet resultSet = stmt.executeQuery();
+            HashMap<Integer, Conseiller> conseillers = new HashMap<>();
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String nom = resultSet.getString("nom");
+                String prenom = resultSet.getString("prenom");
+                String email = resultSet.getString("email");
+
+                Conseiller conseiller = new Conseiller(id, nom, prenom, email);
+                conseillers.put(conseiller.getId(), conseiller);
+            }
+            return conseillers;
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de trouver le conseiller: " + e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public boolean deleteConseiller(Conseiller conseiller) {
-        return false;
+        String deleteQuery = "DELETE FROM conseiller WHERE id = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(deleteQuery)) {
+            stmt.setInt(1, conseiller.getId());
+            int rowsAff = stmt.executeUpdate();
+            if (rowsAff > 0) {
+                return true;
+            } else {
+                throw new SQLException("Suppression échouée");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppression de client: " + e.getMessage());
+            return false;
+        }
     }
 }
--- src/app/repository/interfaces/ConseillerRepository.java
+++ src/app/repository/interfaces/ConseillerRepository.java
@@ -1,12 +1,15 @@
 package app.repository.interfaces;
 
 import app.model.Conseiller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public interface ConseillerRepository {
     Conseiller createConseiller(Conseiller conseiller);
     Conseiller findConseiller(Integer conseiller_id);
-    List<Conseiller> getAllConseiller();
+    HashMap<Integer, Conseiller> getAllConseiller();
     boolean deleteConseiller(Conseiller conseiller);
 
 }
