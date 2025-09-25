e3a88255049985c54ad80b38459ed1bfaee32825
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-25 16:24:37 +0100

    GDCD-15 Realiser l'ajout et find d'un conseiller pour la couche service

--- src/app/service/impl/ConseillerServiceImpl.java
+++ src/app/service/impl/ConseillerServiceImpl.java
@@ -1,4 +1,53 @@
 package app.service.impl;
 
-public class ConseillerServiceImpl {
+import app.model.Client;
+import app.model.Conseiller;
+import app.repository.interfaces.ConseillerRepository;
+import app.service.interfaces.ConseillerService;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ConseillerServiceImpl implements ConseillerService {
+    private ConseillerRepository conseillerRepository;
+
+    public ConseillerServiceImpl(ConseillerRepository conseillerRepository) {
+        this.conseillerRepository = conseillerRepository;
+    }
+
+    @Override
+    public Conseiller ajouterConseiller(Conseiller conseiller) {
+        if (conseiller == null) {
+            throw new IllegalArgumentException("Le conseiller ne peut pas être null");
+        }
+        try {
+            return this.conseillerRepository.createConseiller(conseiller)
+                    .orElseThrow(() -> new RuntimeException("Impossible d'ajouter le conseiller: " + conseiller.getNom() + conseiller.getPrenom()));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Conseiller findConseiller(Integer conseiller_id) {
+        if (conseiller_id == null) {
+            throw new IllegalArgumentException("Le id conseiller ne peut pas être null");
+        }
+        try {
+            return this.conseillerRepository.findConseiller(conseiller_id)
+                    .orElseThrow(() -> new RuntimeException("Aucun conseiller trouvé avec l'id: " + conseiller_id));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Client> getClientsConseiller(Integer conseiller_id) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Boolean deleteConsiller(Integer conseiller_id) {
+        return null;
+    }
 }
--- src/app/service/interfaces/ConseillerService.java
+++ src/app/service/interfaces/ConseillerService.java
@@ -1,4 +1,14 @@
 package app.service.interfaces;
 
+import app.model.Client;
+import app.model.Conseiller;
+
+import java.util.HashMap;
+import java.util.List;
+
 public interface ConseillerService {
+    Conseiller ajouterConseiller(Conseiller conseiller);
+    Conseiller findConseiller(Integer conseiller_id);
+    List<Client> getClientsConseiller(Integer conseiller_id);
+    Boolean deleteConsiller(Integer conseiller_id);
 }
